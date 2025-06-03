699c12000697883c3861c3d93cadfcadf2b43c72
Gavin Fifer <fiferg@oregonstate.edu>
2025-06-02 21:39:38 -0700

    another force website update

--- javascriptFiles/customerOperations.js
+++ javascriptFiles/customerOperations.js
@@ -1,6 +1,6 @@
 import { supabase } from './supabaseConfig.js'
 
-export const CustomerOperations = {
+export const CustomerOperations = { // Temp comment
     async addCustomer(customerData) {
         const { data, error } = await supabase
             .from('Customers')
